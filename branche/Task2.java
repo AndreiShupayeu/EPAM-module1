82bb9e31622ac03315bfded8999c9e372c876cf8
a.shupayeu <andreishupaevqa@gmail.com>
2022-09-18 13:27:22 +0300

    Не измения логику добавил вывод на экран информационные подсказки

--- branche/Task2.java
+++ branche/Task2.java
@@ -1,25 +1,28 @@
 package by.epam.module1.branche;
 //Найти max{min(a, b), min(c, d)}.
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Укажите два числа из которых программа выберет минимальное:");
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
+        double min1 = Math.min(a, b);
+        System.out.println("Минимальное число: " + min1);
+
+        System.out.println("Укажите еще два числа из которых программа выберет минимальное:");
         double c = scanner.nextDouble();
         double d = scanner.nextDouble();
-
-        double min1 = Math.min(a, b);
         double min2 = Math.min(c, d);
+        System.out.println("Минимальное число: " + min2);
+
         double max = Math.max(min1, min2);
-
-        System.out.println(max);
-
-
+        System.out.println("А максимальным из этих двух минимумов будет число: "+max);
     }
+}
 
-    }
+
 
